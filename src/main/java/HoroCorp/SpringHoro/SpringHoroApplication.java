5e7ea39f9fb8d3263bfc62fbd4b995285d67509d
leo <guirado.leo@gmail.com>
2022-08-06 16:53:21 +0200

    Spring REST

---
+++ src/main/java/HoroCorp/SpringHoro/SpringHoroApplication.java
@@ -0,0 +1,13 @@
+package HoroCorp.SpringHoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHoroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHoroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/HoroCorp/SpringHoro/SpringHoroApplicationTests.java
@@ -0,0 +1,13 @@
+package HoroCorp.SpringHoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHoroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
